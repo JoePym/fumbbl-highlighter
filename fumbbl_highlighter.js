7506e4b847d6d2111968883948eaa2e2419494e0
Joe Pym <hitonagashi@gmail.com>
2013-02-25 22:00:23 -0500

    Initial push of the highlighter

---
+++ fumbbl_highlighter.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.matchrecord .race').highlight('Legend', 'legend');
+  $('.matchrecord .race').highlight('Super Star', 'superstar');
+  $('.matchrecord .race').highlight('Emerging Star', 'emergingstar');
+  $('.matchrecord .race').highlight('Experienced', 'experienced');
+  $('.matchrecord .race').highlight('Veteran', 'veteran');
+  $('.matchrecord .race').highlight('Rookie', 'rookie');
+});
