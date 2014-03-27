d4b281486c544a037e4171a6da47e0d4da5cd669
Tuomas Puranen <tuomas.puranen@iki.fi>
2014-03-27 18:30:33 +0200

    Added highlighting of the coach rank Star
    The (Star) rank is now also highlighted.
    Including the opening parenthesis to not mess with Super/Emerging Star
    matches. (I was not determined enough to figure out an easy way to go
    around the  javascript's missing regexp lookbehind functionality to
    accomplish a perfect regexp match.)

--- fumbbl_highlighter.js
+++ fumbbl_highlighter.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $('.matchrecord .race').highlight('Legend', 'legend');
   $('.matchrecord .race').highlight('Super Star', 'superstar');
+  $('.matchrecord .race').highlight('[\(]Star', 'star');
   $('.matchrecord .race').highlight('Emerging Star', 'emergingstar');
   $('.matchrecord .race').highlight('Experienced', 'experienced');
   $('.matchrecord .race').highlight('Veteran', 'veteran');
